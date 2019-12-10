327eed459fdb189bccc31799e284f1ef80d8ecb8
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-10 17:35:30 -0500

    completes design spec stretch goal

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const NumberButton = (props) => {
+  if (props.button === "0") {
+    return (
+        <button className="number_button calc_button number_zero">
+          {props.button}
+        </button>
+    );
+  }
   return (
-    <button className="number_button">
+    <button className="number_button calc_button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.button}
     </button>
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <button className="operator_button">
+    <button className="operator_button calc_button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.button.char}
     </button>
